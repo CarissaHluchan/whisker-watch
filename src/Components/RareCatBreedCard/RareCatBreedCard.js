a355905c52f70c5212e67b1ee3dcd53fe0f5ded6
Carissa Hluchan <carissahluchan@gmail.com>
2024-09-16 12:14:33 -0600

    Add: more to RareCatBreed component

--- src/Components/RareCatBreedCard/RareCatBreedCard.js
+++ src/Components/RareCatBreedCard/RareCatBreedCard.js
@@ -5,7 +5,7 @@ function RareCatBreedCard({ id, key, name, image }) {
         return (
             <div className='rare-cat-breed-card-wrapper' id={id} key={key}>
                 <img src={image} alt={name} className='rare-cat-breed-img'/>
-                <h2 className='rare-cat-breed-name'>{name}</h2>
+                <h3 className='rare-cat-breed-name'>{name}</h3>
             </div>
         )
 }
--- src/Components/RareCatBreeds/RareCatBreeds.js
+++ src/Components/RareCatBreeds/RareCatBreeds.js
@@ -7,7 +7,7 @@ function RareCatBreeds({ rareCatBreeds }) {
 
     const catBreedCards = rareCatBreeds.map(rareCatBreed => {
         return (
-            <RareCatBreedCard 
+            <RareCatBreedCard
                 id={rareCatBreed.id}
                 key={rareCatBreed.id}
                 name={rareCatBreed.name}
@@ -18,7 +18,19 @@ function RareCatBreeds({ rareCatBreeds }) {
 
     return (
         <main className='rare-cat-breed-main'>
-            {catBreedCards}
+            <div className='rare-cat-breed-header'>
+                <div className='rare-cat-breed-title-wrapper'>
+                    <h2 className='rare-cat-breed-title'>Rare Cat breeds</h2>
+                    <p className='rare-cat-breed-instructions'>Explore rare cat breeds.
+                        Scroll through and click on a cat to learn more about that breed.
+                        Discover their temperament, weight, lifespan and more.
+                        Add breeds you like to a list of favorites and learn more about them.
+                    </p>
+                </div>
+                <button className='my-favorite-cats-button'>My favorite cats</button>
+            </div>
+            <div className='rare-cat-breed-container'>{catBreedCards}</div>
+            {/* <button className='see-all-cat-breed-button'>See all cat breeds</button> */}
         </main>
     )
 }
