8f459e29f1b307ce62279ca6731cae00c76f050f
Carissa Hluchan <carissahluchan@gmail.com>
2024-09-18 17:04:12 -0600

    Clean: up FavoriteCatBreedCard.js

--- src/Components/CatBreedCard/CatBreedCard.js
+++ src/Components/CatBreedCard/CatBreedCard.js
@@ -1,6 +1,5 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-
 import './CatBreedCard.css';
 
 function CatBreedCard({ id, name, image }) {
--- src/Components/FavoriteCatBreedCard/FavoriteCatBreedCard.js
+++ src/Components/FavoriteCatBreedCard/FavoriteCatBreedCard.js
@@ -1,40 +1,34 @@
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import './FavoriteCatBreedCard.css';
-import { Link } from "react-router-dom";
 
 function FavoriteCatBreedCard({ id, name, image, removeFromFavoriteCatBreeds }) {
+  const handleRemoveClick = (event) => {
+    event.preventDefault();
+    removeFromFavoriteCatBreeds(id);
+  };
 
-    const handleRemoveClick = (event) => {
-        event.preventDefault();
-        removeFromFavoriteCatBreeds(id)
-    }
-
-    return (
-        <Link
-            to={`/catBreed/${id}`}
-            className='favorite-cat-breed-card-wrapper'
-            id={id}
-        >
-            <h3 className='favorite-cat-breed-name'>{name}</h3>
-            <img
-                src={image}
-                alt={name}
-                className='favorite-cat-breed-img'
-            />
-            <button
-                className='remove-from-favorites-button'
-                onClick={handleRemoveClick}
-            >
-                Remove from Favorites
-            </button>
-        </Link>
-    )
+  return (
+    <div className="favorite-cat-breed-card">
+      <Link to={`/catBreed/${id}`} className="favorite-cat-breed-card-link">
+        <h3 className="favorite-cat-breed-name">{name}</h3>
+        <img src={image} alt={name} className="favorite-cat-breed-img" />
+      </Link>
+      <button
+        className="remove-from-favorites-button"
+        onClick={handleRemoveClick}
+      >
+        Remove from Favorites
+      </button>
+    </div>
+  );
 }
-
-export default FavoriteCatBreedCard;
 
 FavoriteCatBreedCard.propTypes = {
-    image: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    id: PropTypes.string,
-}
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  removeFromFavoriteCatBreeds: PropTypes.func.isRequired,
+};
+
+export default FavoriteCatBreedCard;
