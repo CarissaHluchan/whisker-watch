80ad8dc59528cf96a31353d74a50448062a0712d
Carissa Hluchan <carissahluchan@gmail.com>
2024-09-18 17:00:41 -0600

    Clean: up CatBreedCard.js

--- src/Components/CatBreedCard/CatBreedCard.js
+++ src/Components/CatBreedCard/CatBreedCard.js
@@ -1,29 +1,21 @@
 import PropTypes from 'prop-types';
-import './CatBreedCard.css';
 import { Link } from 'react-router-dom';
 
+import './CatBreedCard.css';
+
 function CatBreedCard({ id, name, image }) {
-
     return (
-        <Link
-            to={`/catBreed/${id}`}
-            className='rare-cat-breed-card-wrapper'
-            id={id}
-        >
-            <img
-                src={image}
-                alt={name}
-                className='rare-cat-breed-img'
-            />
-            <h3 className='rare-cat-breed-name'>{name}</h3>
-        </Link>
-    )
-}
-
-export default CatBreedCard;
-
-CatBreedCard.propTypes = {
+      <Link to={`/catBreed/${id}`} className='cat-breed-card' id={id}>
+        <img src={image} alt={name} className='cat-breed-img' />
+        <h3 className='cat-breed-name'>{name}</h3>
+      </Link>
+    );
+  }
+  
+  CatBreedCard.propTypes = {
     image: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
     id: PropTypes.string,
-}
+  };
+  
+  export default CatBreedCard;
