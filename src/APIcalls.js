3aefb1a89cba32192c1a3c950786839a2ab1ece7
Carissa Hluchan <carissahluchan@gmail.com>
2024-09-18 19:37:02 -0600

    Clean: up aip call for compair

--- src/APIcalls.js
+++ src/APIcalls.js
@@ -21,19 +21,14 @@ const apiKey = process.env.REACT_APP_CAT_API_KEY;
 export async function getCats() {
   try {
     const response = await fetch(`https://api.thecatapi.com/v1/breeds?api_key=${apiKey}`);
-
     if (!response.ok) {
-      // Throw an error if the response status is not OK
       throw new Error('We\'re sorry, there was an error fetching your cats. Please try again later.');
     }
-
-    const data = await response.json(); // Convert the response to JSON
-    console.log(data, 'GET DATA'); // Log the data for debugging
-    return data; // Return the data to be used by the calling code
-
+    const data = await response.json();
+    console.log(data, 'GET DATA'); 
+    return data; 
   } catch (error) {
-    // Handle and log the error
     console.error('ERROR FETCHING DATA:', error.message);
-    throw error; // Re-throw the error if needed for further handling by the caller
+    throw error;
   }
 }
