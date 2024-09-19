0eb97b17324681a37196584686366ff8b31ed686
Carissa Hluchan <carissahluchan@gmail.com>
2024-09-18 19:40:35 -0600

    Fix: changed API calls to async/await

--- src/APIcalls.js
+++ src/APIcalls.js
@@ -1,23 +1,5 @@
 const apiKey = process.env.REACT_APP_CAT_API_KEY;
 
-// export function getCats() {
-//     return fetch(`https://api.thecatapi.com/v1/breeds?api_key=${apiKey}`)
-//       .then(response => {
-//         if (!response.ok) {
-//           throw new Error('We\'re sorry, there was an error fetching your cats. Please try again later.')
-//         }
-//         return response.json();
-//       })
-//       .then(data => {
-//         console.log(data, 'GET DATA')
-//         return data;
-//       })
-//       .catch(error => {
-//         console.error('ERROR FETCHING DATA:', error)
-//         throw error
-//       });
-// }
-
 export async function getCats() {
   try {
     const response = await fetch(`https://api.thecatapi.com/v1/breeds?api_key=${apiKey}`);
